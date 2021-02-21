7c59603e9a3a5ce5d210178fd473b7cc9c4295d2
Nate <nathaniel29651@gmail.com>
2021-02-21 08:55:50 -0500

    Initial Commit

---
+++ FancyList/FancyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancyListApp.swift
+//  FancyList
+//
+//  Created by Larry Mcdowell on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct FancyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
